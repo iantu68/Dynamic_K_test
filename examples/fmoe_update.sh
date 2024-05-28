9fccf25ca5e8d9babdf88527494af0f87bc6cc2d
s10727128 <62416987+s10727128@users.noreply.github.com>
2024-05-28 23:39:12 +0800

    Update fmoe_update.sh

--- examples/fmoe_update.sh
+++ examples/fmoe_update.sh
@@ -1,3 +1,7 @@
-cp ../fmoe/transformer.py /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoe/
-cp ../fmoe/layers.py /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoe/
-cp ../fmoe/functions.py /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoe/
+cp ../fmoe/transformer.py /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoe
+cp ../fmoe/layers.py /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoe
+cp ../fmoe/functions.py /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoe
+
+# cp /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoetransformer.py ../fmoe/
+# cp /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoelayers.py ../fmoe/
+# cp /home/anaconda3/envs/fmoe/lib/python3.10/site-packages/fastmoe-1.1.0-py3.10-linux-x86_64.egg/fmoefunctions.py ../fmoe/
